72944d9b11d6173040bb354884002406cd8f754c
danilbobkov <danil.rend.bobkov@gmail.com>
2026-03-29 14:31:53 +0300

    Add files via upload

---
+++ practicums/PR1/Labs/LR8/TestApp/BackpackApp/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace BackpackApp.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Cost { get; set; }
+    }
+}
