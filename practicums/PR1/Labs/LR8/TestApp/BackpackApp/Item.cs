ff5f35c34247fb459bca600318edede9fdf6c07e
danilbobkov <danil.rend.bobkov@gmail.com>
2026-03-29 14:34:00 +0300

    Delete practicums/PR1/Labs/LR8 directory

--- practicums/PR1/Labs/LR8/TestApp/BackpackApp/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BackpackApp.Models
-{
-    public class Item
-    {
-        public string Name { get; set; }
-        public int Weight { get; set; }
-        public int Cost { get; set; }
-    }
-}
