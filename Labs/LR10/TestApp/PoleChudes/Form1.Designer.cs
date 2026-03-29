4ce40de628dc06a35ff12a64e046df9b1b730a67
danilbobkov <danil.rend.bobkov@gmail.com>
2026-03-29 15:52:58 +0300

    Add files via upload

---
+++ Labs/LR10/TestApp/PoleChudes/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace PoleChudes
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.FlowLayoutPanel flowLetters;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblInfo;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.flowLetters = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // btnNewGame
+            this.btnNewGame.Location = new System.Drawing.Point(12, 12);
+            this.btnNewGame.Size = new System.Drawing.Size(100, 30);
+            this.btnNewGame.Text = "Новая игра";
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+
+            // btnUndo
+            this.btnUndo.Location = new System.Drawing.Point(120, 12);
+            this.btnUndo.Size = new System.Drawing.Size(100, 30);
+            this.btnUndo.Text = "Отмена";
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+
+            // btnCheck
+            this.btnCheck.Location = new System.Drawing.Point(230, 12);
+            this.btnCheck.Size = new System.Drawing.Size(100, 30);
+            this.btnCheck.Text = "Проверить";
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+
+            // flowLetters
+            this.flowLetters.Location = new System.Drawing.Point(12, 60);
+            this.flowLetters.Size = new System.Drawing.Size(320, 150);
+
+            // txtResult
+            this.txtResult.Location = new System.Drawing.Point(12, 220);
+            this.txtResult.Size = new System.Drawing.Size(320, 23);
+
+            // lblInfo
+            this.lblInfo.Location = new System.Drawing.Point(12, 250);
+            this.lblInfo.Size = new System.Drawing.Size(300, 23);
+
+            // Form1
+            this.ClientSize = new System.Drawing.Size(350, 300);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.flowLetters);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblInfo);
+            this.Text = "Поле чудес";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Labs/LR10/TestApp/PoleChudes/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PoleChudes
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=DESKTOP-3CVUA48\SQLEXPRESS;Initial Catalog=PoleChudesDB;Integrated Security=True";
+
+        string currentWord = "";
+        List<char> shuffledLetters = new List<char>();
+        Stack<string> history = new Stack<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            currentWord = GetRandomWord().ToUpper();
+            shuffledLetters = Shuffle(currentWord.ToList());
+
+            flowLetters.Controls.Clear();
+            txtResult.Text = "";
+            history.Clear();
+
+            foreach (char c in shuffledLetters)
+            {
+                Button btn = new Button();
+                btn.Text = c.ToString();
+                btn.Width = 40;
+                btn.Height = 40;
+                btn.Click += Letter_Click;
+                flowLetters.Controls.Add(btn);
+            }
+
+            lblInfo.Text = $"Длина слова: {currentWord.Length}";
+        }
+
+        private string GetRandomWord()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT TOP 1 Word FROM Words ORDER BY NEWID()", conn);
+                return cmd.ExecuteScalar().ToString();
+            }
+        }
+
+        private List<char> Shuffle(List<char> list)
+        {
+            Random rnd = new Random();
+            return list.OrderBy(x => rnd.Next()).ToList();
+        }
+
+        private void Letter_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            history.Push(txtResult.Text);
+
+            txtResult.Text += btn.Text;
+            btn.Enabled = false;
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            if (history.Count > 0)
+            {
+                txtResult.Text = history.Pop();
+
+                foreach (Button btn in flowLetters.Controls)
+                    btn.Enabled = true;
+
+                // заново блокируем уже использованные буквы
+                foreach (char c in txtResult.Text)
+                {
+                    foreach (Button btn in flowLetters.Controls)
+                    {
+                        if (btn.Text == c.ToString() && btn.Enabled)
+                        {
+                            btn.Enabled = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string userWord = txtResult.Text.ToUpper().Replace('Ё', 'Е');
+            string correctWord = currentWord.Replace('Ё', 'Е');
+
+            if (userWord == correctWord)
+                MessageBox.Show("Правильно!");
+            else
+                MessageBox.Show("Неправильно!");
+        }
+    }
+}
