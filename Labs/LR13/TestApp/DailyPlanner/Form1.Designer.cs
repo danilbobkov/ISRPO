b6042d6c151e9176fa085fdfe681a41639e113e8
danilbobkov <danil.rend.bobkov@gmail.com>
2026-04-02 22:29:04 +0300

    Add files via upload

---
+++ Labs/LR13/TestApp/DailyPlanner/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DailyPlanner
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.ListBox listBoxNotes;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.DateTimePicker timePicker;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.listBoxNotes = new System.Windows.Forms.ListBox();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+
+            this.SuspendLayout();
+
+            this.monthCalendar1.Location = new System.Drawing.Point(10, 10);
+            this.monthCalendar1.DateChanged += new DateRangeEventHandler(this.monthCalendar1_DateChanged);
+
+            this.listBoxNotes.Location = new System.Drawing.Point(250, 10);
+            this.listBoxNotes.Size = new System.Drawing.Size(300, 200);
+            this.listBoxNotes.SelectedIndexChanged += new EventHandler(this.listBoxNotes_SelectedIndexChanged);
+
+            this.txtNote.Location = new System.Drawing.Point(250, 220);
+            this.txtNote.Size = new System.Drawing.Size(300, 23);
+
+            this.timePicker.Format = DateTimePickerFormat.Time;
+            this.timePicker.ShowUpDown = true;
+            this.timePicker.Location = new System.Drawing.Point(250, 250);
+
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.Location = new System.Drawing.Point(250, 280);
+            this.btnAdd.Click += new EventHandler(this.btnAdd_Click);
+
+            this.btnEdit.Text = "Редактировать";
+            this.btnEdit.Location = new System.Drawing.Point(350, 280);
+            this.btnEdit.Click += new EventHandler(this.btnEdit_Click);
+
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.Location = new System.Drawing.Point(480, 280);
+            this.btnDelete.Click += new EventHandler(this.btnDelete_Click);
+
+            this.ClientSize = new System.Drawing.Size(600, 330);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.listBoxNotes);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.timePicker);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+
+            this.Text = "Заметки";
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Labs/LR13/TestApp/DailyPlanner/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DailyPlanner
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=DESKTOP-3CVUA48\SQLEXPRESS;Database=DailyPlannerDB;Trusted_Connection=True;";
+        int selectedNoteId = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadNotes();
+        }
+
+        private void LoadNotes()
+        {
+            listBoxNotes.Items.Clear();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = @"SELECT Id, NoteDate, NoteText 
+                                 FROM Notes 
+                                 WHERE CAST(NoteDate AS DATE) = @date
+                                 ORDER BY NoteDate";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@date", monthCalendar1.SelectionStart.Date);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (!reader.HasRows)
+                {
+                    listBoxNotes.Items.Add("Нет заметок на этот день");
+                    return;
+                }
+
+                while (reader.Read())
+                {
+                    DateTime dt = (DateTime)reader["NoteDate"];
+                    string text = reader["NoteText"].ToString();
+
+                    string display = $"{dt:HH:mm} - {TrimText(text)}";
+
+                    listBoxNotes.Items.Add(new NoteItem
+                    {
+                        Id = (int)reader["Id"],
+                        Display = display,
+                        FullText = text,
+                        Time = dt
+                    });
+                }
+            }
+        }
+
+        private string TrimText(string text)
+        {
+            return text.Length > 60 ? text.Substring(0, 60) + "..." : text;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNote.Text))
+            {
+                MessageBox.Show("Введите текст заметки!");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                DateTime date = monthCalendar1.SelectionStart.Date + timePicker.Value.TimeOfDay;
+
+                string query = "INSERT INTO Notes (NoteDate, NoteText) VALUES (@date, @text)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@date", date);
+                cmd.Parameters.AddWithValue("@text", txtNote.Text.Trim());
+
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Заметка успешно добавлена!");
+            ClearForm();
+            LoadNotes();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (selectedNoteId == -1)
+            {
+                MessageBox.Show("Сначала выберите заметку!");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtNote.Text))
+            {
+                MessageBox.Show("Введите текст заметки!");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                DateTime date = monthCalendar1.SelectionStart.Date + timePicker.Value.TimeOfDay;
+
+                string query = "UPDATE Notes SET NoteDate=@date, NoteText=@text WHERE Id=@id";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@date", date);
+                cmd.Parameters.AddWithValue("@text", txtNote.Text.Trim());
+                cmd.Parameters.AddWithValue("@id", selectedNoteId);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Заметка обновлена!");
+            ClearForm();
+            LoadNotes();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (selectedNoteId == -1)
+            {
+                MessageBox.Show("Выберите заметку!");
+                return;
+            }
+
+            if (MessageBox.Show("Удалить заметку?", "Подтверждение", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = "DELETE FROM Notes WHERE Id=@id";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", selectedNoteId);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Удалено!");
+            ClearForm();
+            LoadNotes();
+        }
+
+        private void listBoxNotes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxNotes.SelectedItem is NoteItem item)
+            {
+                selectedNoteId = item.Id;
+                txtNote.Text = item.FullText;
+                timePicker.Value = item.Time;
+            }
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            LoadNotes();
+        }
+
+        private void ClearForm()
+        {
+            txtNote.Clear();
+            timePicker.Value = DateTime.Now;
+            selectedNoteId = -1;
+        }
+    }
+
+    public class NoteItem
+    {
+        public int Id { get; set; }
+        public string Display { get; set; }
+        public string FullText { get; set; }
+        public DateTime Time { get; set; }
+
+        public override string ToString()
+        {
+            return Display;
+        }
+    }
+}
